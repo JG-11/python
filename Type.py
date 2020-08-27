0f29b325ef6c7ef10085cec3c18393e4a6a008d6
Genaro Almaraz <jg08950@gmail.com>
2020-08-27 13:52:31 -0500

    Basic Data Types

---
+++ Type.py
@@ -0,0 +1,16 @@
+""" 
+    Basic Data Types
+
+    1. Integers <int>
+    2. Floating-Point Numbers <float>
+    3. Strings <str>
+    4. Boolean <bool>
+"""
+
+print(type(1))
+
+print(type(2.5))
+
+print(type('Hello'))
+
+print(type(3 == 5))
