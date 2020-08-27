c6b38e0294ded6482a7d1be5a3224f2966ce140d
Genaro Almaraz <jg08950@gmail.com>
2020-08-27 13:41:26 -0500

    Operators

---
+++ Operators.py
@@ -0,0 +1,19 @@
+print(3 + 4)
+
+print(7 - 2)
+
+print(10 * 20)
+
+print(5 / 2)
+
+print(5 // 2) # With no decimals
+
+print(9 % 2)
+
+print(3 ** 2)
+
+print(2 > 5)
+
+print(10 < 20)
+
+print(11 == 1)
