5bf4be7cc8489b7c28a1da614e7f3e0b8d4b8dd9
Genaro Almaraz <jg08950@gmail.com>
2020-08-28 21:04:28 -0500

    Sets

---
+++ Data_Structures/Sets.py
@@ -0,0 +1,14 @@
+s = set([1, 2, 3])
+t = set([3, 4, 5])
+
+print(type(s))
+
+print(s.union(t))
+
+print(s.intersection(t))
+
+print(s.difference(t))
+
+print(4 in s.intersection(t))
+
+print(5 in s)
