7dc3d833b6835f140dd5696b65440a5179ace3ca
Genaro Almaraz <jg08950@gmail.com>
2020-08-28 21:00:09 -0500

    Tuples

---
+++ Data_Structures/Tuples.py
@@ -0,0 +1,13 @@
+# Tuples - Immutable sequence that can contain any type of element
+
+a = 1, 2, 3, 'hola'
+print(type(a), a)
+
+b = 1,
+print(type(b), b)
+
+c = (1, 2, 3, 4)
+print(type(c), c)
+
+d = tuple([1.3, 'a', 2])
+print(type(d), d)
